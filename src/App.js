0ce331886de213a42aba1325f7990f04922b5b7d
AnnaSM94 <161344697+AnnaSM94@users.noreply.github.com>
2024-05-09 11:08:14 +0100

    Added links and hosted project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,34 @@ export default function App() {
     <div className="App">
       <header className="App-header"></header>
       <h1>Hello</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/AnnaSM94"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Anna Serra
+          </a>
+          , code is open-sourced on{" "}
+          <a
+            href="https://github.com/AnnaSM94/dictionary-react-asm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://dictionary-react-asm.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
